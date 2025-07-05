238bc8007bbbd4cd5d567e1ba745ef3671b31378
Mehfuza Walim <mehfuza@live.com>
2025-07-05 13:02:38 +0530

    Add files via upload

---
+++ Exercise2Task1.py
@@ -0,0 +1,8 @@
+#Program to take number from user and display whether it is even or odd
+num=int(input("Enter a number: "))
+print("\n")
+if num%2==0:
+	print(num, " is an even number.")
+else:
+	print(num, " is an odd number.")
+
---
+++ Exercise2Task2.py
@@ -0,0 +1,6 @@
+#Program to display sum of numbers from 1 to 50 using for loop.
+sum=0
+for num in range(51): #iterates through 00 to 50
+	sum=sum + num
+print("The sum of integers from 1 to 50 is: ", sum)
+
